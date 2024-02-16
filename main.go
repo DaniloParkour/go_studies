98d7f89fec820ec42a42a1896e295bd6f89742d1
DaniloParkour <danilomlcarvalho@gmail.com>
2024-02-16 16:51:10 -0300

    Using * and &. Working with refferences, show addres, content of poniter and othes

--- main.go
+++ main.go
@@ -21,8 +21,36 @@ func main() {
 	account2.numAgence = 5090
 	account2.balance = 9600.75
 
-	fmt.Println("\nShow account2 first var value and second the content of pointer")
+	fmt.Println("\nShow account2: first var value and the second is the content of pointer")
 	fmt.Println(account2)
 	fmt.Println(*account2)
+
+	account3 := Account{owner: "Heitor", numAgence: 589, numAccount: 123456, balance: 125500.50}
+	account4 := Account{owner: "Heitor", numAgence: 589, numAccount: 123456, balance: 125500.50}
+	fmt.Println("\nWe working with values and they're has the same values. Is equals =", account3 == account4)
+	account4.owner = "Heitor Vidal"
+	fmt.Println("\nWe working with values and they're has one different valuee. Is equals =", account3 == account4)
+
+	//Usint refference addres, que equals'll check the addres and not the content
+	var account5 *Account
+	var account6 *Account
+	account5 = new(Account)
+	account6 = new(Account)
+
+	account5.owner = "Helena"
+	account5.numAccount = 112233
+	account5.numAgence = 5090
+	account5.balance = 9600.75
+
+	account6.owner = "Helena"
+	account6.numAccount = 112233
+	account6.numAgence = 5090
+	account6.balance = 9600.75
+
+	fmt.Println("\nWe working with reffecrence address and they're has the same values on fields but we're comparing the refference address and the're differente. Is equals =", account5 == account6)
+	fmt.Println("\nCheck below the addres of account5 and account6.")
+	fmt.Println(&account5)
+	fmt.Println(&account6)
+	fmt.Println("\nNow we keep working with 2 diffentent refference address with the same values on fields but now we're comparing values on each addres. Is equals =", *account5 == *account6)
 
 }
